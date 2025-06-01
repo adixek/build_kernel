6d44c83658cebc697faea26b079645a0bd895cec
adixek <kaz14adi@gmail.com>
2025-06-01 05:08:18 +0000

    Update alioth.py

--- device/xiaomi/alioth.py
+++ device/xiaomi/alioth.py
@@ -1,8 +1,8 @@
 from build_kernel.utils.device import register_device
 from device.xiaomi.sm8250 import XiaomiSM8250Device
 
-class XiaomiAliothDevice(XiaomiSM8250Device):
-	PRODUCT_DEVICE = "alioth"
+class XiaomiPipaDevice(XiaomiSM8250Device):
+	PRODUCT_DEVICE = "pipa"
 	TARGET_KERNEL_FRAGMENTS = ["vendor/xiaomi/sm8250-common.config", f"vendor/xiaomi/{PRODUCT_DEVICE}.config"]
 	AB_OTA_UPDATER = True
 
